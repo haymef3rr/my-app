a7097d352b64923264ffe1db0aac88dbd0df187a
jaimef3rr <ferrer.jaime@hcx.com.ph>
2023-03-02 14:59:51 +0800

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Login } from "./Login";
-import { Register } from "./Register";
 
 function App() {
   const [currentForm, setCurrentForm] = useState("login");
